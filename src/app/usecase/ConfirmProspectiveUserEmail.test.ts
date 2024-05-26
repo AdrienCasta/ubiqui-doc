732c433be2fdc0826c53ef010cfde727d1b137cd
Adrien Castagliola <a.castagliola@smartway.ai>
2024-05-26 19:58:00 +0200

    ♻️ refactor ConfirmProspectiveUserEmail

--- src/app/usecase/ConfirmProspectiveUserEmail.test.ts
+++ src/app/usecase/ConfirmProspectiveUserEmail.test.ts
@@ -9,37 +9,34 @@ import InMemoryEmailConfirmationTokenRepository from '../../infra/repository/InM
 import ConfirmProspectiveUserEmail from './ConfirmProspectiveUserEmail';
 import SystemClock from '../../shared/SystemClock';
 import EmailVerificationTokenService from '../services/EmailVerificationTokenService';
+import UserRepository from '../../domain/repository/UserRepository';
+import EmailConfirmationTokenRepository from '../../domain/repository/EmailConfirmationTokenRepository';
 
 describe('ConfirmProspectiveUserEmail', () => {
+  const clock = new SystemClock();
   let sendConfirmationEmailService: FakeSendConfirmationEmailService;
-  let userRepository: InMemoryUserRepository;
-  let emailConfirmationTokenRepository: InMemoryEmailConfirmationTokenRepository;
-  let registerProspectiveUserCommandHandler: RegisterProspectiveUserCommandHandler;
+  let userRepository: UserRepository;
+  let emailConfirmationTokenRepository: EmailConfirmationTokenRepository;
   let registerProspectiveUser: RegisterProspectiveUser;
   let confirmProspectiveUserEmail: ConfirmProspectiveUserEmail;
-  let emailVerificationTokenService: EmailVerificationTokenService;
-
-  const clock = new SystemClock();
 
   beforeEach(async () => {
     sendConfirmationEmailService = new FakeSendConfirmationEmailService();
-    emailVerificationTokenService = new EmailVerificationTokenService(
-      () => 'token',
-      () => new Date(),
-      clock,
-    );
     userRepository = new InMemoryUserRepository();
     emailConfirmationTokenRepository =
       new InMemoryEmailConfirmationTokenRepository();
-    registerProspectiveUserCommandHandler =
+
+    registerProspectiveUser = new RegisterProspectiveUser(
       new RegisterProspectiveUserCommandHandler(
         userRepository,
         emailConfirmationTokenRepository,
         sendConfirmationEmailService,
-        emailVerificationTokenService,
-      );
-    registerProspectiveUser = new RegisterProspectiveUser(
-      registerProspectiveUserCommandHandler,
+        new EmailVerificationTokenService(
+          () => 'token',
+          () => new Date(),
+          clock,
+        ),
+      ),
     );
     confirmProspectiveUserEmail = new ConfirmProspectiveUserEmail(
       userRepository,
@@ -58,7 +55,9 @@ describe('ConfirmProspectiveUserEmail', () => {
         password: 'SecurePass123!',
       },
     };
+
     await registerProspectiveUser.execute(registerProspectiveUserCommand);
+
     const confirmationResult = await confirmProspectiveUserEmail.execute(
       registerProspectiveUserCommand.user.email,
       'token',
@@ -102,9 +101,6 @@ describe('ConfirmProspectiveUserEmail', () => {
     expect(confirmationResult.isFailure()).toBe(true);
   });
   it('invites user to confirm email because token is not expired yet', async () => {
-    const oneMinuteAgo = new Date(clock.now().getTime() - 60000);
-    const oneMinuteFromNow = new Date(clock.now().getTime() + 60000);
-
     const registerProspectiveUserCommand = {
       user: {
         id: randomUUID(),
@@ -113,26 +109,16 @@ describe('ConfirmProspectiveUserEmail', () => {
         lastName: 'Doe',
         password: 'SecurePass123!',
       },
-      confirmation: {
-        token: 'token',
-        expiresAt: oneMinuteAgo,
-      },
     };
 
-    const registerProspectiveUserAgain = {
-      user: registerProspectiveUserCommand.user,
-      confirmation: {
-        ...registerProspectiveUserCommand.confirmation,
-        expiresAt: oneMinuteFromNow,
-      },
-    };
     await registerProspectiveUser.execute(registerProspectiveUserCommand);
-    const test = await registerProspectiveUser.execute(
-      registerProspectiveUserAgain,
-    );
-    expect(test.isFailure()).toBe(true);
-    console.log(test.getError());
-    expect(test.getError()).toBeInstanceOf(
+
+    const attemptToRegisterAgain = await registerProspectiveUser.execute({
+      user: registerProspectiveUserCommand.user,
+    });
+
+    expect(attemptToRegisterAgain.isFailure()).toBe(true);
+    expect(attemptToRegisterAgain.getError()).toBeInstanceOf(
       RegisterProspectiveUserCommandHandlerErrors.EmailUnconfirmedError,
     );
   });
